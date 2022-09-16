7d0a5243a20b8e5fbfcffd2aed7c6d79ea9fae14
carlitossaul <carlitossaul.h@gmail.com>
2022-09-16 18:57:45 -0300

    s

--- demo.js
+++ demo.js
@@ -1,4 +1,11 @@
 let test = "probando123"
-console.log(test)
-test = "perro"
-let cat = " gato"
+//console.log(test)
+
+function test2 (){
+    return 8
+}
+console.log(test2())
+
+let test3 = new test2()
+
+console.log(test3)
