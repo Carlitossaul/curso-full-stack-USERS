4ef3e4939a3fd7847b7cf63ea3f7f3c3a3748314
carlitossaul <carlitossaul.h@gmail.com>
2022-09-17 19:26:05 -0300

    s

--- demo.js
+++ demo.js
@@ -1,11 +1,13 @@
-let test = "probando123"
-//console.log(test)
 
-function test2 (){
-    return 8
-}
-console.log(test2())
+    // var reves = ""
+    // let array = [1,2,3,4,5,6,7,8,9]
+    // for(let i = array.length-1; i >=0; i--){
+    //     reves = reves + array[i]
+    // }
 
-let test3 = new test2()
 
-console.log(test3)
+    // console.log(reves)
+
+    let palabra = "hola"
+   palabra = palabra.split("").reverse().join("")
+    console.log(palabra)
